391a15c6c82b2bc3e6d27d0eca97c43b0c592c54
Caro Pohn <carolinapohn@gmail.com>
2023-09-11 19:59:34 -0300

    Update Main.cpp

--- PruebaAlgebra/Main.cpp
+++ PruebaAlgebra/Main.cpp
@@ -12,21 +12,25 @@ void DrawInstructions();
 
 void main()
 {
-    Vector2 magnitude1Pos = { 10,30 };
+    Vector2 magnitudeTextPos = { 10, 30 };
+    Vector2 vectorATextPos = { 10, 50 };
+    Vector2 vectorBTextPos = { 10, 70 };
+    Vector2 vectorCTextPos = { 10, 90 };
+
+    Vector3 startPos = { 0.0f, 0.0f, 0.0f };
 
     //Vector A datos
-    Vector3 startPos = { 0.0f, 0.0f, 0.0f };
-    Vector3 endPos = { 5.0f, 0.0f, 0.0f };
+    Vector3 vectorA = { 5.0f, 0.0f, 0.0f };
     Color color = RED;
 
-    Vector3 perpendicular = { 0.0f, 0.0f, 0.0f }; //Vector B
-    Vector3 vertical = { 0.0f, 0.0f, 0.0f }; //Vector C
+    Vector3 vectorB = { 0.0f, 0.0f, 0.0f }; //Vector B
+    Vector3 vectorC = { 0.0f, 0.0f, 0.0f }; //Vector C
 
     int userInput;
 
-    float magnitude = 1; //A magnitude
-    float magnitude2 = 1; //B magnitude
-    float magnitude3 = 1; //C magnitude
+    float magnitudeA = 1; //A magnitude
+    float magnitudeB = 1; //B magnitude
+    float magnitudeC = 1; //C magnitude
 
     bool starting = true;
 
@@ -43,13 +47,13 @@ void main()
 
     SetTargetFPS(60);
 
-    GetMagnitude(endPos, magnitude); //A magnitude
-    GetFirstPerpendicular(endPos, perpendicular); //Crea vector B
+    GetMagnitude(vectorA, magnitudeA); //A magnitude
+    GetFirstPerpendicular(vectorA, vectorB); //Crea vector B
 
-    SetNewMagnitude(perpendicular, magnitude); //Normaliza y multiplica magnitud Vector B por la magnitud Vector A
-    GetMagnitude(perpendicular, magnitude2); //Magnitud actual de Vector B
+    SetNewMagnitude(vectorB, magnitudeA); //Normaliza y multiplica magnitud Vector B por la magnitud Vector A
+    GetMagnitude(vectorB, magnitudeB); //Magnitud actual de Vector B
 
-    GetVertical(endPos, vertical); //Crea Vector C
+    GetVertical(vectorA, vectorC); //Crea Vector C
     
     camera.position = startPos;
 
@@ -59,8 +63,8 @@ void main()
 
     camera.target = startPos;
 
-    cout << "\nMagnitud A: " << magnitude << endl;
-    cout << "\nMagnitud B: " << magnitude2 << endl;
+    cout << "\nMagnitud A: " << magnitudeA << endl;
+    cout << "\nMagnitud B: " << magnitudeB << endl;
 
     while (!WindowShouldClose())
     {
@@ -68,34 +72,34 @@ void main()
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        DrawText(TextFormat("Vector magnitude: %01i", magnitude), magnitude1Pos.x, magnitude1Pos.y, 15, BLACK);
+        DrawText(TextFormat("Vector A and B magnitude: %01i", magnitudeA), magnitudeTextPos.x, magnitudeTextPos.y, 15, BLACK);
+        DrawText("Vector A: RED", vectorATextPos.x, vectorATextPos.y, 15, BLACK);
+        DrawText("Vector B: GREEN", vectorBTextPos.x, vectorBTextPos.y, 15, BLACK);
+        DrawText("Vector C: BLUE", vectorCTextPos.x, vectorCTextPos.y, 15, BLACK);
 
         int key;
 
         BeginMode3D(camera);
 
-        DrawLine3D(startPos, endPos, color);
-        DrawLine3D(startPos, perpendicular, GREEN);
-        DrawLine3D(startPos, vertical, BLUE);
+        DrawLine3D(startPos, vectorA, color);
+        DrawLine3D(startPos, vectorB, GREEN);
+        DrawLine3D(startPos, vectorC, BLUE);
 
         EndMode3D();
 
         if (starting)
         {
-            //DrawText(TextFormat("Ingrese el valor deseado: "), ((float)GetScreenWidth() / 2), ((float)GetScreenHeight() / 2), 13, BLACK);
             DrawInstructions();
 
             key = GetCharPressed() - 48;
 
-            //DrawText(TextFormat("Key: %01i", key), ((float)GetScreenWidth() / 2), ((float)GetScreenHeight() / 2), 13, BLACK);
-
             if (key > 1 && key < 10)
             {
-                SetNewMagnitude(vertical, magnitude / key);
-                GetMagnitude(vertical, magnitude3);
+                SetNewMagnitude(vectorC, magnitudeA / key); //Normaliza el Vector C y le da la magnitud deseada por el usuario
+                GetMagnitude(vectorC, magnitudeC); //Vector C magnitude actual
 
                 cout << "\nInput: " << key << endl;
-                cout << "\nMagnitud C: " << magnitude3 << endl;
+                cout << "\nMagnitud C: " << magnitudeC << endl;
 
                 starting = false;
             }
@@ -108,14 +112,6 @@ void main()
                 camera.target = startPos;
             }
         }
-   
-        //BeginMode3D(camera);
-
-        //DrawLine3D(startPos, endPos, color);
-        //DrawLine3D(startPos, perpendicular, GREEN);
-        //DrawLine3D(startPos, vertical, BLUE);
-
-        //EndMode3D();
 
         DrawFPS(10, 10);
         EndDrawing();
